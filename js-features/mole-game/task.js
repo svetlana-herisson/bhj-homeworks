5e7c07d0a919497646a16a6e25dbf90a34066f3a
Svetlana <svetlana.herissonn@gmail.com>
2025-04-14 11:35:28 +0300

    на первую проверку

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,3 +1,34 @@
 const deadDisplay = document.getElementById("dead");
 const lostDisplay = document.getElementById("lost");
+const holes = document.querySelectorAll('.hole');
 
+let counterDead = 0;
+let counterLost = 0;
+
+function getHole(index) {
+    return document.getElementById(`hole${index}`);
+}
+
+function killMole() {
+    for (let i = 1; i <= 9; i++) {
+        if (getHole(i).classList.contains('hole_has-mole')) {
+            deadDisplay.textContent = ++counterDead;
+        } else {
+            lostDisplay.textContent = ++counterLost;
+        }
+
+        if (counterDead === 10 ) {
+        endGame("Победа!");
+            } else if (counterLost === 5) {
+        endGame("Игра окончена. Вы проиграли");
+        }
+    }
+}
+
+function endGame(massage) {
+        alert(massage);
+        counterDead = 0;
+        counterLost = 0;
+        deadDisplay.textContent = counterDead;
+        lostDisplay.textContent = counterLost;
+}
